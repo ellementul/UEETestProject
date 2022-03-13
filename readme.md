b89b22adf17a493c1b85a26805d5c55bfbd49c2f
ellementul <ananev.ivan.ur@ya.ru>
2022-03-13 11:29:35 +0500

    Added sync tickers

--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 # UEE Test Project
 ## TODO
-1. Sync tickers without db
-1. Server enviroment events(Tiker module)
-1. Db module(event DB connect)(NoDB - database)
-1. Store module
-1. Save ticks in store module(server time as object in store)
-1. Load ticks from store module
+1. Sleep for ticker
+2. Small delta and big delta
+3. Server enviroment events(Tiker module)
+4. Db module(event DB connect)(NoDB - database)
+5. Store module
+6. Save ticks in store module(server time as object in store)
+7. Load ticks from store module
--- src/modules/ticker-module.js
+++ src/modules/ticker-module.js
@@ -1,18 +1,35 @@
 import { Module } from 'uee'
 
+let timerHandler
+
 class Ticker extends Module {
   constructor () {
     super()
+    this.delta = 1000
     this.count = 0
   }
-  defEvents () { return [] }
+  defEvents () { return [{ name: "tick" }] }
 
   sendTick () {
     this.count++
     this.sendEvent({ name: 'tick', payload: { count: this.count } })
+    timerHandler = setTimeout(this.sendTick.bind(this), this.delta)
   }
+
+  sleep () {
+    clearTimeout(timerHandler)
+    timerHandler = setTimeout(this.sendTick.bind(this), this.delta * 2)
+  }
+
   run () {
-    setInterval(this.sendTick.bind(this), 1000)
+    timerHandler = setTimeout(this.sendTick.bind(this), this.delta)
+  }
+
+  tick (payload) {
+    if (payload.count > this.count) {
+      this.count = payload.count
+      this.sleep()
+    }
   }
 }
 
