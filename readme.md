e16948638624ad997ab4cd12d9f3fce97a2c410d
ellementul <ananev.ivan.ur@ya.ru>
2022-03-20 12:22:07 +0500

    Added pixijs

--- readme.md
+++ readme.md
@@ -1,6 +1,22 @@
 # UEE Test Project
-## TODO
-1. Db save in file
-2. Store module
-3. Save ticks in store module(server time as object in store)
-4. Load ticks from store module
+### Chat
+1. Chat without history
+2. Connect pixijs
+3. Render text pixijs
+4. Chat show module(recive message)
+5. Create new message module(only send message, in message count server and time)
+6. Realtime chat(show message what other person write)
+7. Create state module, State module send message "Update State of Module", "Load State of Module"
+8. Create history module, get state and compare old state
+9. History module send event "Save DB object", write object name in const entity 
+10. History module get message "Load State of Module" and send "Get DB Object", write object name in const entity 
+11. Add in payload and signature Event const "Entity"
+12. Create DB module, get message  "Get DB  Object", "Save DB  Object", get object name in const entity 
+13. DB send event "Update DB Object", write object name in const entity
+14. Chat show extends state module
+15. Chat with history
+16. Chat with Auth
+17. Create several chats
+
+### First game
+1. Zero-cross game
--- src/modules/ticker-widget.js
+++ src/modules/ticker-widget.js
@@ -1,3 +1,4 @@
+import * as PIXI from 'pixi.js'
 import { Module } from 'uee'
 
 class TickerWidget extends Module {
@@ -14,6 +15,35 @@ class TickerWidget extends Module {
 
   run () {
     document.getElementById("headticker").innerHTML = 'Runnig widget:'
+    const app = new PIXI.Application()
+
+    // The application will create a canvas element for you that you
+      // can then insert into the DOM.
+      document.body.appendChild(app.view);
+
+      // load the texture we need
+      app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+
+        // This creates a texture from a 'bunny.png' image.
+        const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+        // Setup the position of the bunny
+        bunny.x = app.renderer.width / 2;
+        bunny.y = app.renderer.height / 2;
+
+        // Rotate around the center
+        bunny.anchor.x = 0.5;
+        bunny.anchor.y = 0.5;
+
+        // Add the bunny to the scene we are building.
+        app.stage.addChild(bunny);
+
+        // Listen for frame updates
+        app.ticker.add(() => {
+            // each frame we spin the bunny around a bit
+            bunny.rotation += 0.01;
+        });
+    });
   }
 
   tick ({ count, bigCount }) {
